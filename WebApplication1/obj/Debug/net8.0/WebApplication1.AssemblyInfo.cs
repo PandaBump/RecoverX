d4d0e581c69b9155b275f6700e9a7577c5ca0604
PandaBump <stokesk6128@gmail.com>
2026-02-03 18:20:50 -0500

    Add: - Scripts - Relocate appsettings.json to improve structure - Worker layer - Relocate .cs files (AuditLog, FileRecord, RecoveryJob, Backup) to improve structure - Initialize .csproj files for remaining layers

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8e22d2541084f3649631cdc44d5d7773157542")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
